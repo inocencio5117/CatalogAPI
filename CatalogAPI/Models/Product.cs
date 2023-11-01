4f296a95e4c0fbdb6fe8e8c42d48c4196f386bab
Vinicius Inocêncio <vinicius@inocencio.dev>
2023-11-01 11:36:07 -0300

    Adding models and Entity framework packages

---
+++ CatalogAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public float? Stock {  get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+    }
+}
