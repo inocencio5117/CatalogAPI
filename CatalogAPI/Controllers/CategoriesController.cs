9a739749f096167a27914f404235a595b12ca2b8
Vinicius Inocêncio <vinicius@inocencio.dev>
2023-11-07 16:29:29 -0300

    Creating CategoriesController CRUD

---
+++ CatalogAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,83 @@
+﻿using CatalogAPI.Context;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> Get()
+        {
+            return _context.Categories.ToList();
+        }
+
+        [HttpGet("{id:int}", Name="ObtainCategory")]
+        public ActionResult<Category> Get(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(p => p.CategoryId == id);
+
+            if (category is null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpGet("products")]
+        public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
+        {
+            return _context.Categories.Include(p =>  p.Products).ToList();
+        }
+
+        [HttpPost]
+        public ActionResult Post(Category category)
+        {
+            if (category is null)
+                return BadRequest();
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObtainCategory",
+                new { id = category.CategoryId }, category);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Category category)
+        {
+            if (id != category.CategoryId)
+                return BadRequest();
+
+            _context.Entry(category).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(category);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Category> Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(p => p.CategoryId == id);
+
+            if (category is null)
+                return NotFound();
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return Ok(category);
+        }
+    }
+}
