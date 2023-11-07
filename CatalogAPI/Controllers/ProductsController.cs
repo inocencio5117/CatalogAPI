767a24b65a1cc3c7cd3aaf2c65800eff858ac5ec
Vinicius Inocêncio <vinicius@inocencio.dev>
2023-11-07 15:51:02 -0300

    Creating ProductsController CRUD

---
+++ CatalogAPI/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using CatalogAPI.Context;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var products = _context.Products.ToList();
+            if (products is null)
+            {
+                return NotFound();
+            }
+            return products;
+        }
+
+        [HttpGet("{id:int}", Name="ObtainProduct")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Product product)
+        {
+            if (product is null)
+                return BadRequest();
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObtainProduct",
+                new { id = product.ProductId }, product);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Product product)
+        {
+            if (id != product.ProductId)
+                return BadRequest();
+
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (product is null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+    }
+}
